de998dcb7542872fc09585394e7a1b99edc287ba
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2025-11-13 18:04:36 -0300

    recipe page

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AuthPage from './pages/AuthPage';
+import Recipe from './pages/Recipe';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/auth" element={<AuthPage />} />
-        {/* Defina outras rotas aqui */}
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/recipe" element={<Recipe />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
