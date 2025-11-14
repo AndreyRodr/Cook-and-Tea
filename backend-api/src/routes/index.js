fce8769fa7f8315f48899f75550b3dee75c5097c
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2025-11-14 19:19:47 -0300

    aaaa

--- backend-api/src/routes/index.js
+++ backend-api/src/routes/index.js
@@ -3,7 +3,6 @@ import userRoutes from "./userRoutes.js"
 import recipeRoutes from './recipeRoutes.js';
 import avaliationRoutes from './avaliationRoutes.js'
 import authRoutes from './authRoutes.js';
-import recipeImageRoutes from './recipeImageRoutes.js';
 
 const router = Router();
 
@@ -11,7 +10,6 @@ router.use('/recipes',recipeRoutes);
 router.use('/users', userRoutes);
 router.use('/avaliations', avaliationRoutes);
 router.use('/auth', authRoutes);
-router.use('/recipe-images', recipeImageRoutes);
 
 router.get('/', (req, res) => {
     res.status(200).json({message: "API Rodando"});
--- backend-api/src/routes/recipeRoutes.js
+++ backend-api/src/routes/recipeRoutes.js
@@ -2,10 +2,6 @@ import { Router } from "express";
 const recipeRoutes = Router();
 import * as recipeController from '../controllers/recipeController.js';
 import { protect } from '../middleware/authMiddleware.js'
-import multer from 'multer';
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
 //GET /api/recipes/
 recipeRoutes.get('/', recipeController.getAllRecipes);
@@ -26,7 +22,6 @@ recipeRoutes.get('/:id', recipeController.getRecipeById);
 recipeRoutes.post(
     '/',
     protect,
-    upload.array('recipeImages', 10),
     recipeController.createRecipe);
 
 //PUT /api/recipes/:id
